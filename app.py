4037d2a0a140b55064c719027feadb4c5b16bee6
Mustafa Bekir Yek <126047546+Mustafa-termux@users.noreply.github.com>
2025-07-23 22:12:15 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
-from flask import Flask, request, jsonify
-import openai
-import os
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-@app.route("/")
-def home():
-    return "BekirGPT Render Sunucusu Çalışıyor!"
-
-@app.route("/soru", methods=["POST"])
-def cevap():
-    data = request.json
-    soru = data.get("soru", "")
-    # Burada openai çağrısı veya BekirGPT fonksiyonunu kullanabilirsin
-    yanit = f"Senin soruna cevap: {soru}"  # Demo amaçlı
-    return jsonify({"cevap": yanit})
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
